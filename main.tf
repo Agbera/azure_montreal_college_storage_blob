89879679a1a6a0b275f27109413c9855343f38fb
Agbera <126923646+Agbera@users.noreply.github.com>
2023-10-20 20:10:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,13 @@ resource "azurerm_resource_group" "montrealcollege" {
   location = "West Europe"
 }
 
-resource "azurerm_kubernetes_cluster" "MontrealCollege" {
-  name                = "montrealcollege-aks1"
+resource "azurerm_kubernetes_cluster" "montrealcollege" {
+  for_each = toset(local.cluster_names)
+
+  name                = "${each.value}-aks"
   location            = azurerm_resource_group.montrealcollege.location
   resource_group_name = azurerm_resource_group.montrealcollege.name
-  dns_prefix          = "montrealcollegeaks1"
+  dns_prefix          = "${each.value}-aks-dns"
 
   default_node_pool {
     name       = "default"
@@ -29,12 +31,10 @@ resource "azurerm_kubernetes_cluster" "MontrealCollege" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.montrealcollege.kube_config.0.client_certificate
-  sensitive = true
+output "client_certificates" {
+  value = { for cluster in azurerm_kubernetes_cluster.montrealcollege : cluster.name => cluster.kube_config.0.client_certificate }
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.montrealcollege.kube_config_raw
-  sensitive = true
+output "kube_configs" {
+  value = { for cluster in azurerm_kubernetes_cluster.montrealcollege : cluster.name => cluster.kube_config_raw }
 }
